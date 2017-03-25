11b48712a015d1773592940266126074b83c97a1
Dan Vega <danvega@gmail.com>
2017-03-25 10:50:50 -0400

    The first step in this tutorial is to create a controller and service without annotating them.

--- src/main/java/com/therealdanvega/controller/StereotypeController.java
+++ src/main/java/com/therealdanvega/controller/StereotypeController.java
@@ -1,7 +1,5 @@
 package com.therealdanvega.controller;
 
-import org.springframework.stereotype.Controller;
-
 public class StereotypeController {
 
 }
--- src/main/java/com/therealdanvega/service/StereotypeService.java
+++ src/main/java/com/therealdanvega/service/StereotypeService.java
@@ -1,7 +1,5 @@
 package com.therealdanvega.service;
 
-import org.springframework.stereotype.Service;
-
 public class StereotypeService {
 
 }
